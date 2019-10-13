ec5d81c845a3ed5f31c8ab470a7d88a0d30ee1b3
Nicolás Luna <nlunaxx@gmail.com>
2019-10-13 11:37:00 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://nlunaxx:svX2xy4O5vBNIHZH@cluster0-fuwia.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB; // es un url custom
